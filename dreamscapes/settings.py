381f3193cd615042e890400f774a503a9c49517d
monicathomas8 <monicathomas8@icloud.com>
2025-04-21 21:05:54 +0100

    changed email settings and added email verification on sign up

--- dreamscapes/settings.py
+++ dreamscapes/settings.py
@@ -69,7 +69,8 @@ ACCOUNT_LOGOUT_REDIRECT_URL = '/'  # Redirect to homepage after logout
 # Allauth settings
 ACCOUNT_SIGNUP_FIELDS = ['email*', 'password1*', 'password2*']
 ACCOUNT_LOGIN_METHODS = {'email'}
-ACCOUNT_EMAIL_VERIFICATION = "none"
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+
 
 
 MIDDLEWARE = [
