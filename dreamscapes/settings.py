0ef4ff97ef932adce9b2abff58ddd9a5661cc260
monicathomas8 <monicathomas8@icloud.com>
2025-04-21 12:03:47 +0100

    fixed bug of image of artwork not showing

--- dreamscapes/settings.py
+++ dreamscapes/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'artwork_images'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
