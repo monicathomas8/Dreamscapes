1366ad98582726cf598ba1e7b9ba41d8d6f620a7
monicathomas8 <monicathomas8@icloud.com>
2025-04-23 17:38:41 +0100

    built contat us page in home app

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import contact_us
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('contact/', contact_us, name='contact_us'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 
 
 def home(request):
+    """
+    Render the home page.
+    """
     return render(request, 'home/index.html')
+
+
+def contact_us(request):
+    """
+    Render the contact us page.
+    """
+    return render(request, 'home/contact_us.html')
+
