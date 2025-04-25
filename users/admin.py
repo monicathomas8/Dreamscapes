083537038a09251504c1683360de238e276e0889
monicathomas8 <monicathomas8@icloud.com>
2025-04-25 12:57:25 +0100

    Updated authentication pages with Bootstrap styling

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'is_artist')
+    search_fields = ('user__username', 'user__email')
+    ordering = ('is_artist',)
---
+++ users/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        widgets = {
+            'password': forms.PasswordInput(),  # Hides password input
+        }
