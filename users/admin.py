ac6b4e43845dde36f2463db49bb9b21e52c1ab33
monicathomas8 <monicathomas8@icloud.com>
2025-04-25 13:52:36 +0100

    Updated admin panel to display and edit the 'is_artist' field in UserProfile

--- users/admin.py
+++ users/admin.py
@@ -4,6 +4,10 @@ from .models import UserProfile
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
+    """
+    Admin interface for UserProfile model.
+    """
     list_display = ('user', 'is_artist')
     search_fields = ('user__username', 'user__email')
-    ordering = ('is_artist',)
+    list_editable = ('is_artist',)
+    ordering = ('is_artist', 'user')
