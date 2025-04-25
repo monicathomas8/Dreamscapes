a8161fb12164c669823acd424e841463e5500279
monicathomas8 <monicathomas8@icloud.com>
2025-04-25 10:16:09 +0100

    created artistBio model and admin access

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ArtistBio
 
-# Register your models here.
+
+@admin.register(ArtistBio)
+class ArtistBioAdmin(admin.ModelAdmin):
+    list_display = ("user", "name", "updated_at")
+    search_fields = ("name", "user__username")
--- home/models.py
+++ home/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class ArtistBio(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="artist_bio"
+    )
+    name = models.CharField(
+        max_length=255,
+        blank=False,
+        null=False,
+        help_text="Enter your Artist name."
+    )
+    interests = models.TextField(
+        blank=True,
+        help_text="List your artistic interests."
+    )
+    background = models.TextField(help_text="Tell your story as an artist.")
+    inspiration = models.TextField(
+        blank=False,
+        null=False,
+        help_text="What inspires your work?"
+    )
+    work_experience = models.TextField(
+        blank=True,
+        help_text="Share your past projects or exhibitions."
+    )
+    profile_image = CloudinaryField('image', blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Bio"
