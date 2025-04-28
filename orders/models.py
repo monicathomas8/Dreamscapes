3e75be5ba21fc9b85c097cf10fd747b9ccc0c4e5
monicathomas8 <monicathomas8@icloud.com>
2025-04-28 08:21:32 +0100

    updated order model to include delievery address

--- orders/models.py
+++ orders/models.py
@@ -18,6 +18,15 @@ class Order(models.Model):
         ],
     )
 
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+    email = models.EmailField(blank=True)
+    address_line_1 = models.CharField(max_length=100, blank=True)
+    address_line_2 = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+    postal_code = models.CharField(max_length=20, blank=True)
+    country = models.CharField(max_length=50, blank=True)
+
     def __str__(self):
         return f"Order #{self.id} by {self.user.username}"
 
